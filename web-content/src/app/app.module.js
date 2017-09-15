1c29578b9845dedc2f225051ebbe8af8558dcf06
cyclic-reference <alex@cyclicreference.space>
2017-09-15 14:48:27 -0500

    web contents

---
+++ web-content/src/app/app.module.js
@@ -0,0 +1,60 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
+var router_1 = require("@angular/router");
+var app_component_1 = require("./app.component");
+var http_1 = require("@angular/http");
+var angular2_ui_switch_1 = require("angular2-ui-switch");
+var message_component_1 = require("./messages/message.component");
+var switch_component_1 = require("./switch/switch.component");
+var host_service_1 = require("./session/host.service");
+var session_service_1 = require("./session/session.service");
+var message_service_1 = require("./messages/message.service");
+var window_1 = require("./util/window");
+var auth_service_1 = require("./auth/auth.service");
+var auth_guard_1 = require("./auth/auth.guard");
+var login_component_1 = require("./auth/login.component");
+var base_component_1 = require("./base.component");
+var appRoutes = [
+    { path: '', component: base_component_1.BaseComponent, canActivate: [auth_guard_1.AuthGuard] },
+    { path: 'butt', component: message_component_1.MessageComponent, canActivate: [auth_guard_1.AuthGuard] },
+    { path: 'login', component: login_component_1.LoginComponent }
+];
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule,
+            http_1.HttpModule,
+            angular2_ui_switch_1.UiSwitchModule,
+            router_1.RouterModule.forRoot(appRoutes)
+        ],
+        exports: [
+            router_1.RouterModule
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            message_component_1.MessageComponent,
+            switch_component_1.SwitchComponent,
+            base_component_1.BaseComponent,
+            login_component_1.LoginComponent
+        ],
+        bootstrap: [app_component_1.AppComponent],
+        providers: [host_service_1.HostService, session_service_1.SessionService, message_service_1.MessageService, window_1.WindowRef, auth_service_1.AuthService, auth_guard_1.AuthGuard]
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ web-content/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+import {AppComponent} from "./app.component";
+import {HttpModule} from "@angular/http";
+import {UiSwitchModule} from "angular2-ui-switch";
+import {MessageComponent} from "./messages/message.component";
+import {SwitchComponent} from "./switch/switch.component";
+import {HostService} from "./session/host.service";
+import {SessionService} from "./session/session.service";
+import {MessageService} from "./messages/message.service";
+import {WindowRef} from "./util/window";
+import {AuthService} from "./auth/auth.service";
+import {AuthGuard} from "./auth/auth.guard";
+import {LoginComponent} from "./auth/login.component";
+import {BaseComponent} from "./base.component";
+
+const appRoutes = [
+  {path: '', component: BaseComponent, canActivate: [AuthGuard]},
+  {path: 'butt', component: MessageComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent}
+];
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    UiSwitchModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+    AppComponent,
+    MessageComponent,
+    SwitchComponent
+    ,
+    BaseComponent,
+    LoginComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [HostService, SessionService, MessageService, WindowRef, AuthService, AuthGuard]
+})
+export class AppModule {
+}
