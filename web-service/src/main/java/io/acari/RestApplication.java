6b1a4fe4eddc824da75df906572318c3adbaf1e1
cyclic-reference <alex@cyclicreference.space>
2017-09-18 08:16:35 -0500

    really simple rest application

--- web-service/src/main/java/io/acari/RestApplication.java
+++ web-service/src/main/java/io/acari/RestApplication.java
@@ -1,7 +1,16 @@
 package io.acari;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.Instant;
 
 @ApplicationPath("")
 public class RestApplication {
+
+    @Path("")
+    @GET
+    public String getMessage(){
+        return "@ " + Instant.now() + " Vagrant is AWESOME!\n";
+    }
 }
